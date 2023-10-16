e8f578fb2dd2addc2cac585184ac1220c0426118
TimGeel1998 <140500672+TimGeel1998@users.noreply.github.com>
2023-10-16 23:30:16 +0200

    Update HellDiverOccupation.lua

--- HellDivers/media/lua/shared/NPCs/HellDiverOccupation.lua
+++ HellDivers/media/lua/shared/NPCs/HellDiverOccupation.lua
@@ -28,7 +28,7 @@ addProfession('HellDiver', {
         FullSuit = {"Base.Boilersuit_Flying"},
         Shoes =  {"Base.Shoes_ArmyBoots"},
     },
-    traits = {"Desensitized","Brave"},
+    traits = {"Desensitized","Brave","adrenalinejunkie"},
 })
 
 --         TorsoExtra = {"Base.Vest_BulletArmy"},
